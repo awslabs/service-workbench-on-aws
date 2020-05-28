3955c2874295440edb5ab78ca6d0b326d8f0a076
Sanket Dharwadkar <sdharwad@amazon.com>
2020-05-28 09:33:56 -0400

    get css as npm package

--- main/solution/ui/src/index.js
+++ main/solution/ui/src/index.js
@@ -20,7 +20,7 @@ import pluginRegistry from './plugins/plugin-registry';
 import 'typeface-lato';
 import './css/basscss-important.css';
 import './css/semantic.min.css';
-import './css/animate.css';
+import 'animate.css/animate.css';
 import 'toastr/build/toastr.css';
 import 'react-table/react-table.css';
 import './css/index.css';
