dcce0631bdb5a84eb1b7392e41e19c9e897146bf
Judson Neer <judneer@amazon.com>
2020-06-01 20:26:32 -0700

    Add a CI/CD process that uploads configs to S3 instead of requiring them to be committed to the repo.

--- main/cicd/cicd-pipeline/scripts/deploy-github-token.bash
+++ main/cicd/cicd-pipeline/scripts/deploy-github-token.bash
@@ -2,6 +2,9 @@
 set -e
 
 
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+
 stack_name="$1"
 token_name="$2"
 
@@ -9,6 +12,7 @@ token_name="$2"
 token=$(aws ssm get-parameter --name ${token_name} --with-decryption --output text --query Parameter.Value 2> /dev/null || echo 'not-found')
 
 if [[ $token != 'not-found' ]]; then
+  echo "Deploying CI/CD stack ${stack_name} with Github token from parameter ${token_name}"
   aws cloudformation update-stack \
     --stack-name ${stack_name} \
     --capabilities CAPABILITY_IAM \
