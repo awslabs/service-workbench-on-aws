32e2194299d69b73125884010c4c932ca52bb574
Ludo <lddsctx@gmail.com>
2020-06-10 18:58:55 +0200

    Script to test scalability of workspaces creation/start/access/stop/termination.

---
+++ scripts/load-test-workspaces/load-test-workspaces.utils.js
@@ -0,0 +1,53 @@
+const https = require('https');
+
+async function makeApiRequest(options, data) {
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, res => {
+            const body = [];
+            if (res.statusCode < 200 || res.Status > 299) {
+                reject(new Error(`request failed: ${res.StatusCode}`));
+            }
+            res.on('data', chunk => {
+                body.push(chunk);
+            });
+
+            res.on('end', () => {
+                resolve({ statusCode: res.statusCode, headers: res.headers, body: Buffer.concat(body).toString() });
+            });
+        });
+
+        req.on('error', error => {
+            console.error(`error: ${error}`);
+            reject(error);
+        });
+
+        if (data) {
+            options.headers['Content-Length'] = Buffer.byteLength(data);
+            req.write(data);
+        }
+
+        req.end();
+    });
+}
+
+function sleep(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
+
+function filterSetCookie(setCookie) {
+    const cookie = setCookie.split('; ').reduce((cookieString, elt) => {
+        const param = elt.split('=');
+        if (param.length === 2 && cookieString.length > 0) {
+            cookieString += '; ' + param[0] + '=' + param[1];
+        }
+        else if (param.length === 2) {
+            cookieString = param[0] + '=' + param[1];
+        }
+        return cookieString;
+    }, []);
+    return cookie;
+}
+
+module.exports = { makeApiRequest, sleep, filterSetCookie };
