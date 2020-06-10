fbb4e81d0726180f464aab739609ef1839a3b6dc
Sanket Dharwadkar <sdharwad@amazon.com>
2020-06-09 22:38:06 -0400

    replacing blog link with relevant comment

--- addons/addon-base-raas-ui/packages/base-raas-ui/src/parts/projects/ProjectConfigure.js
+++ addons/addon-base-raas-ui/packages/base-raas-ui/src/parts/projects/ProjectConfigure.js
@@ -403,7 +403,7 @@ class ProjectConfigure extends React.Component {
   }
 }
 
-// see https://medium.com/@mweststrate/mobx-4-better-simpler-faster-smaller-c1fbc08008da
+// Using the MobX 4 way to use decorators without decorator syntax
 decorate(ProjectConfigure, {
   formProcessing: observable,
   modalOpen: observable,
