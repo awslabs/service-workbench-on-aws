3931ad2a36ca503cd5f165be831708c951bbd949
SanketD92 <sdharwad@amazon.com>
2020-07-15 14:55:51 -0400

    fix for CICD s3 bucket permission

--- main/cicd/cicd-pipeline/scripts/upload-env-config.bash
+++ main/cicd/cicd-pipeline/scripts/upload-env-config.bash
@@ -5,8 +5,9 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 
-deployment_bucket="${1}"
-environment_name="${2}"
+aws_profile="${1}"
+deployment_bucket="${2}"
+environment_name="${3}"
 
 config_filename="../../config/settings/${environment_name}.yml"
 config_s3_path="s3://${deployment_bucket}/settings/"
@@ -14,5 +15,5 @@ config_s3_path="s3://${deployment_bucket}/settings/"
 
 if [[ -f "${config_filename}" ]]; then
   echo "Uploading ${config_filename} to ${config_s3_path}"
-  aws s3 cp --sse aws:kms "${config_filename}" "${config_s3_path}"
+  aws --profile "${aws_profile}" s3 cp --sse aws:kms "${config_filename}" "${config_s3_path}"
 fi
