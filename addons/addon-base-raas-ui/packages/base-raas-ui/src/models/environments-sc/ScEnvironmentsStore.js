f04d5f766efb5f1ff47bf899d6eac5867bf5c361
Yanyu Zheng <yz2690@columbia.edu>
2020-10-05 09:38:24 -0400

    Revert "Update RStudio check"
    This reverts commit 3e01a54504f6d096d4fe9e978cea6962e684171b.

--- addons/addon-base-raas-ui/packages/base-raas-ui/src/models/environments-sc/ScEnvironmentsStore.js
+++ addons/addon-base-raas-ui/packages/base-raas-ui/src/models/environments-sc/ScEnvironmentsStore.js
@@ -166,7 +166,7 @@ const ScEnvironmentsStore = BaseStore.named('ScEnvironmentsStore')
         if (output.OutputKey === 'Ec2WorkspaceInstanceId' || output.OutputKey === 'NotebookInstanceName') {
           result = true;
         }
-        if (output.OutputKey === 'MetaConnection1Type' && output.OutputValue === 'RStudio') {
+        if (output.OutputKey === 'MetaConnection1Type') {
           isRStudio = true;
         }
       });
