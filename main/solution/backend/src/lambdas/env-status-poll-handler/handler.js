d74c796b9ef6defb17f4082ee43325c9bbf444df
zheyanyu <zheyanyu@amazon.com>
2020-10-06 21:42:09 -0400

    Add status poll logic, update permissions needed

--- main/solution/backend/src/lambdas/env-status-poll-handler/handler.js
+++ main/solution/backend/src/lambdas/env-status-poll-handler/handler.js
@@ -1,8 +1,19 @@
-exports.handler = function(event, context, callback) {
-  console.log(event);
-  console.log(context);
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello, World',
-  });
+const ServicesContainer = require('@aws-ee/base-services-container/lib/services-container');
+const { registerServices } = require('@aws-ee/base-services/lib/utils/services-registration-util');
+const { getSystemRequestContext } = require('@aws-ee/base-services/lib/helpers/system-context');
+
+const pluginRegistry = require('./plugins/plugin-registry');
+
+const handler = async () => {
+  const container = new ServicesContainer(['settings', 'log']);
+  // registerServices - Registers services by calling each service registration plugin in order.
+  await registerServices(container, pluginRegistry);
+  await container.initServices();
+  const environmentScService = await container.find('environmentScService');
+  const userContext = getSystemRequestContext();
+  const envData = await environmentScService.pollAndSyncWsStatus(userContext);
+  return { statusCode: 200, body: envData };
 };
+
+// eslint-disable-next-line import/prefer-default-export
+module.exports.handler = handler;
