e65a48cdada88e7403848dcf726e4020468fe8b6
Yanyu Zheng <yz2690@columbia.edu>
2020-11-12 18:23:54 -0500

    fix: cyclic dependency (#207)
    * fix: cyclic dependency between environment-sc-service and environment-mount-service
    
    Co-authored-by: zheyanyu <zheyanyu@amazon.com>

--- addons/addon-base-raas/packages/base-raas-services/lib/environment/__tests__/environment-mount-service.test.js
+++ addons/addon-base-raas/packages/base-raas-services/lib/environment/__tests__/environment-mount-service.test.js
@@ -46,11 +46,8 @@ const EnvironmentMountService = require('../environment-mount-service');
 
 describe('EnvironmentMountService', () => {
   let service = null;
-  let studyService = null;
-  let storageGatewayService = null;
   let environmentScService = null;
   let iamService = null;
-  const context = { principalIdentifier: { uid: 'u-daffyduck' } };
 
   beforeEach(async () => {
     // Initialize services container and register dependencies
@@ -70,85 +67,8 @@ describe('EnvironmentMountService', () => {
 
     // Get instance of the service we are testing
     service = await container.find('environmentMountService');
-    studyService = await container.find('studyService');
-    storageGatewayService = await container.find('storageGatewayService');
     environmentScService = await container.find('environmentScService');
     iamService = await container.find('iamService');
-  });
-
-  describe('updateStudyFileMountIPAllowList', () => {
-    it('should not call storageGatewayService if mounted studies do not have file share', async () => {
-      // BUILD
-      const studiesList = [
-        { id: 'study1', resources: [{ arn: 'study1-s3-path-arn' }] },
-        { id: 'study2', resources: [{ arn: 'study2-s3-path-arn' }] },
-      ];
-      studyService.listByIds = jest.fn().mockResolvedValueOnce(studiesList);
-      const environment = { studyIds: ['study1', 'study2'] };
-      // OPERATE
-      await service.updateStudyFileMountIPAllowList(context, environment, {});
-      // CHECK
-      expect(storageGatewayService.updateFileSharesIPAllowedList).not.toHaveBeenCalled();
-    });
-
-    it('should call storageGatewayService with correct parameter for Add IP to allow list', async () => {
-      // BUILD
-      const studiesList = [
-        { id: 'study1', resources: [{ arn: 'study1-s3-path-arn', fileShareArn: 'study1-file-share-arn' }] },
-        { id: 'study2', resources: [{ arn: 'study2-s3-path-arn', fileShareArn: 'study2-file-share-arn' }] },
-      ];
-      studyService.listByIds = jest.fn().mockResolvedValueOnce(studiesList);
-      const environment = { studyIds: ['study1', 'study2'] };
-      const ipAllowListAction = { ip: '12.23.34.45', action: 'ADD' };
-      // OPERATE
-      await service.updateStudyFileMountIPAllowList(context, environment, ipAllowListAction);
-      // CHECK
-      expect(storageGatewayService.updateFileSharesIPAllowedList).toHaveBeenCalledWith(
-        ['study1-file-share-arn', 'study2-file-share-arn'],
-        '12.23.34.45',
-        'ADD',
-      );
-    });
-
-    it('should not call storageGatewayService when ip it not in environment or ipAllowListAction', async () => {
-      // BUILD
-      const studiesList = [
-        { id: 'study1', resources: [{ arn: 'study1-s3-path-arn', fileShareArn: 'study1-file-share-arn' }] },
-        { id: 'study2', resources: [{ arn: 'study2-s3-path-arn', fileShareArn: 'study2-file-share-arn' }] },
-      ];
-      studyService.listByIds = jest.fn().mockResolvedValueOnce(studiesList);
-      const environment = {
-        studyIds: ['study1', 'study2'],
-        outputs: [{ OutputKey: 'Ec2WorkspaceInstanceId', OutputValue: 'some-ec2-instance-id' }],
-      };
-      const ipAllowListAction = { action: 'REMOVE' };
-      // OPERATE
-      await service.updateStudyFileMountIPAllowList(context, environment, ipAllowListAction);
-      // CHECK
-      expect(storageGatewayService.updateFileSharesIPAllowedList).not.toHaveBeenCalled();
-    });
-
-    it('should call storageGatewayService with correct parameter for REMOVE IP to allow list', async () => {
-      // BUILD
-      const studiesList = [
-        { id: 'study1', resources: [{ arn: 'study1-s3-path-arn', fileShareArn: 'study1-file-share-arn' }] },
-        { id: 'study2', resources: [{ arn: 'study2-s3-path-arn', fileShareArn: 'study2-file-share-arn' }] },
-      ];
-      studyService.listByIds = jest.fn().mockResolvedValueOnce(studiesList);
-      const environment = {
-        studyIds: ['study1', 'study2'],
-        outputs: [{ OutputKey: 'Ec2WorkspacePublicIp', OutputValue: '34.45.56.67' }],
-      };
-      const ipAllowListAction = { action: 'REMOVE' };
-      // OPERATE
-      await service.updateStudyFileMountIPAllowList(context, environment, ipAllowListAction);
-      // CHECK
-      expect(storageGatewayService.updateFileSharesIPAllowedList).toHaveBeenCalledWith(
-        ['study1-file-share-arn', 'study2-file-share-arn'],
-        '34.45.56.67',
-        'REMOVE',
-      );
-    });
   });
 
   describe('Update paths', () => {
