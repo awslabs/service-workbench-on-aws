76e2f3d7476871d9b54277fc365d5f15e5042d08
SanketD92 <sdharwad@amazon.com>
2020-12-15 13:27:28 -0500

    fix: Upload Files button disappears for R/W users

--- addons/addon-base-raas-ui/packages/base-raas-ui/src/parts/studies/StudyRow.js
+++ addons/addon-base-raas-ui/packages/base-raas-ui/src/parts/studies/StudyRow.js
@@ -99,7 +99,9 @@ class StudyRow extends React.Component {
 
     return (
       <>
-        {study.uploadLocationEnabled && study.access === 'admin' && <UploadStudyFiles studyId={study.id} />}
+        {study.uploadLocationEnabled && (study.access === 'admin' || study.access === 'readwrite') && (
+          <UploadStudyFiles studyId={study.id} />
+        )}
         <Header as="h3" color="blue" className={c('mt0', isSelectable ? 'cursor-pointer' : '')} {...onClickAttr}>
           {study.name}
           <Header.Subheader>
