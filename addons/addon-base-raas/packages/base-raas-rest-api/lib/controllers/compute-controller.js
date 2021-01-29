624d21b6cc2421076e34ee19f081528b8a241fc2
Jeet Nihalani <jeetend@amazon.com>
2021-01-29 11:38:36 -0700

    feat: Remove APIs for built-in workspaces and clarify documentation on the enableBuiltInWorkspaces flag

--- addons/addon-base-raas/packages/base-raas-rest-api/lib/controllers/compute-controller.js
+++ addons/addon-base-raas/packages/base-raas-rest-api/lib/controllers/compute-controller.js
@@ -17,38 +17,38 @@
 
 async function configure(context) {
   const router = context.router();
-  const wrap = context.wrap;
+  // const wrap = context.wrap;
 
-  const computePlatformService = await context.service('computePlatformService');
+  // const computePlatformService = await context.service('computePlatformService');
 
   // ===============================================================
   //  GET /platforms (mounted to /api/compute)
   // ===============================================================
-  router.get(
-    '/platforms',
-    wrap(async (req, res) => {
-      const requestContext = res.locals.requestContext;
-
-      const platforms = await computePlatformService.listPlatforms(requestContext);
-      res.status(200).json(platforms);
-    }),
-  );
-
-  // ===============================================================
-  //  GET /platforms/:id/configurations (mounted to /api/compute)
-  // ===============================================================
-  router.get(
-    '/platforms/:id/configurations',
-    wrap(async (req, res) => {
-      const requestContext = res.locals.requestContext;
-      const id = req.params.id;
-      const platforms = await computePlatformService.listConfigurations(requestContext, {
-        platformId: id,
-        includePrice: true,
-      });
-      res.status(200).json(platforms);
-    }),
-  );
+  // router.get(
+  //   '/platforms',
+  //   wrap(async (req, res) => {
+  //     const requestContext = res.locals.requestContext;
+  //
+  //     const platforms = await computePlatformService.listPlatforms(requestContext);
+  //     res.status(200).json(platforms);
+  //   }),
+  // );
+  //
+  // // ===============================================================
+  // //  GET /platforms/:id/configurations (mounted to /api/compute)
+  // // ===============================================================
+  // router.get(
+  //   '/platforms/:id/configurations',
+  //   wrap(async (req, res) => {
+  //     const requestContext = res.locals.requestContext;
+  //     const id = req.params.id;
+  //     const platforms = await computePlatformService.listConfigurations(requestContext, {
+  //       platformId: id,
+  //       includePrice: true,
+  //     });
+  //     res.status(200).json(platforms);
+  //   }),
+  // );
 
   return router;
 }
