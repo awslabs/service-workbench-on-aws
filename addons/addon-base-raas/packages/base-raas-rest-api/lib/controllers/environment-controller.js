0f2cebb4ca5d2b035fae53115bbf91a9a6a67271
Jeet Nihalani <jeetend@amazon.com>
2021-01-29 11:38:36 -0700

    feat: Remove APIs for built-in workspaces and clarify documentation on the enableBuiltInWorkspaces flag

--- addons/addon-base-raas/packages/base-raas-rest-api/lib/controllers/environment-controller.js
+++ addons/addon-base-raas/packages/base-raas-rest-api/lib/controllers/environment-controller.js
@@ -18,178 +18,178 @@
 
 async function configure(context) {
   const router = context.router();
-  const wrap = context.wrap;
-
-  const [
-    environmentService,
-    environmentKeypairService,
-    environmentUrlService,
-    environmentSpotPriceHistoryService,
-  ] = await context.service([
-    'environmentService',
-    'environmentKeypairService',
-    'environmentUrlService',
-    'environmentSpotPriceHistoryService',
-  ]);
-
-  // ===============================================================
-  //  GET / (mounted to /api/workspaces/built-in)
-  // ===============================================================
-  router.get(
-    '/',
-    wrap(async (req, res) => {
-      const requestContext = res.locals.requestContext;
-
-      const result = await environmentService.list(requestContext);
-      res.status(200).json(result);
-    }),
-  );
-
-  // ===============================================================
-  //  GET /:id (mounted to /api/workspaces/built-in)
-  // ===============================================================
-  router.get(
-    '/:id',
-    wrap(async (req, res) => {
-      const id = req.params.id;
-      const requestContext = res.locals.requestContext;
-
-      const result = await environmentService.mustFind(requestContext, { id });
-      res.status(200).json(result);
-    }),
-  );
-
-  // ===============================================================
-  //  POST / (mounted to /api/workspaces/built-in)
-  // ===============================================================
-  router.post(
-    '/',
-    wrap(async (req, res) => {
-      const requestContext = res.locals.requestContext;
-      const possibleBody = req.body;
-      const result = requestContext.principal.isExternalUser
-        ? await environmentService.createExternal(requestContext, possibleBody)
-        : await environmentService.create(requestContext, possibleBody);
-
-      res.status(200).json(result);
-    }),
-  );
-
-  // ===============================================================
-  //  PUT / (mounted to /api/workspaces/built-in)
-  // ===============================================================
-  router.put(
-    '/',
-    wrap(async (req, res) => {
-      const requestContext = res.locals.requestContext;
-      const possibleBody = req.body;
-      const result = await environmentService.update(requestContext, possibleBody);
-
-      res.status(200).json(result);
-    }),
-  );
-
-  // ===============================================================
-  //  PUT / (mounted to /api/workspaces/built-in)
-  // ===============================================================
-  router.put(
-    '/:id/start',
-    wrap(async (req, res) => {
-      const requestContext = res.locals.requestContext;
-      const id = req.params.id;
-      const operation = 'start';
-      const result = await environmentService.changeWorkspaceRunState(requestContext, { id, operation });
-
-      res.status(200).json(result);
-    }),
-  );
-
-  // ===============================================================
-  //  PUT / (mounted to /api/workspaces/built-in)
-  // ===============================================================
-  router.put(
-    '/:id/stop',
-    wrap(async (req, res) => {
-      const requestContext = res.locals.requestContext;
-      const id = req.params.id;
-      const operation = 'stop';
-      const result = await environmentService.changeWorkspaceRunState(requestContext, { id, operation });
-
-      res.status(200).json(result);
-    }),
-  );
-
-  // ===============================================================
-  //  DELETE /:id (mounted to /api/workspaces/built-in)
-  // ===============================================================
-  router.delete(
-    '/:id',
-    wrap(async (req, res) => {
-      const id = req.params.id;
-      const requestContext = res.locals.requestContext;
-
-      await environmentService.delete(requestContext, { id });
-      res.status(200).json({});
-    }),
-  );
-
-  // ===============================================================
-  //  GET /:id/keypair (mounted to /api/workspaces/built-in)
-  // ===============================================================
-  router.get(
-    '/:id/keypair',
-    wrap(async (req, res) => {
-      const requestContext = res.locals.requestContext;
-      const id = req.params.id;
-
-      const result = await environmentKeypairService.mustFind(requestContext, id);
-
-      res.status(200).json(result);
-    }),
-  );
-
-  // ===============================================================
-  //  GET /:id/password (mounted to /api/workspaces/built-in)
-  // ===============================================================
-  router.get(
-    '/:id/password',
-    wrap(async (req, res) => {
-      const requestContext = res.locals.requestContext;
-      const id = req.params.id;
-
-      const result = await environmentService.getWindowsPasswordData(requestContext, { id });
-
-      res.status(200).json(result);
-    }),
-  );
-
-  // ===============================================================
-  //  GET /:id/url (mounted to /api/workspaces/built-in)
-  // ===============================================================
-  router.get(
-    '/:id/url',
-    wrap(async (req, res) => {
-      const requestContext = res.locals.requestContext;
-      const id = req.params.id;
-      const result = await environmentUrlService.get(requestContext, id);
-
-      res.status(200).json(result);
-    }),
-  );
-
-  // ===============================================================
-  //  GET /pricing/:type (mounted to /api/workspaces/built-in)
-  // ===============================================================
-  router.get(
-    '/pricing/:type',
-    wrap(async (req, res) => {
-      const requestContext = res.locals.requestContext;
-      const type = req.params.type;
-      const result = await environmentSpotPriceHistoryService.getPriceHistory(requestContext, type);
-
-      res.status(200).json(result);
-    }),
-  );
+  // const wrap = context.wrap;
+  //
+  // const [
+  //   environmentService,
+  //   environmentKeypairService,
+  //   environmentUrlService,
+  //   environmentSpotPriceHistoryService,
+  // ] = await context.service([
+  //   'environmentService',
+  //   'environmentKeypairService',
+  //   'environmentUrlService',
+  //   'environmentSpotPriceHistoryService',
+  // ]);
+  //
+  // // ===============================================================
+  // //  GET / (mounted to /api/workspaces/built-in)
+  // // ===============================================================
+  // router.get(
+  //   '/',
+  //   wrap(async (req, res) => {
+  //     const requestContext = res.locals.requestContext;
+  //
+  //     const result = await environmentService.list(requestContext);
+  //     res.status(200).json(result);
+  //   }),
+  // );
+  //
+  // // ===============================================================
+  // //  GET /:id (mounted to /api/workspaces/built-in)
+  // // ===============================================================
+  // router.get(
+  //   '/:id',
+  //   wrap(async (req, res) => {
+  //     const id = req.params.id;
+  //     const requestContext = res.locals.requestContext;
+  //
+  //     const result = await environmentService.mustFind(requestContext, { id });
+  //     res.status(200).json(result);
+  //   }),
+  // );
+  //
+  // // ===============================================================
+  // //  POST / (mounted to /api/workspaces/built-in)
+  // // ===============================================================
+  // router.post(
+  //   '/',
+  //   wrap(async (req, res) => {
+  //     const requestContext = res.locals.requestContext;
+  //     const possibleBody = req.body;
+  //     const result = requestContext.principal.isExternalUser
+  //       ? await environmentService.createExternal(requestContext, possibleBody)
+  //       : await environmentService.create(requestContext, possibleBody);
+  //
+  //     res.status(200).json(result);
+  //   }),
+  // );
+  //
+  // // ===============================================================
+  // //  PUT / (mounted to /api/workspaces/built-in)
+  // // ===============================================================
+  // router.put(
+  //   '/',
+  //   wrap(async (req, res) => {
+  //     const requestContext = res.locals.requestContext;
+  //     const possibleBody = req.body;
+  //     const result = await environmentService.update(requestContext, possibleBody);
+  //
+  //     res.status(200).json(result);
+  //   }),
+  // );
+  //
+  // // ===============================================================
+  // //  PUT / (mounted to /api/workspaces/built-in)
+  // // ===============================================================
+  // router.put(
+  //   '/:id/start',
+  //   wrap(async (req, res) => {
+  //     const requestContext = res.locals.requestContext;
+  //     const id = req.params.id;
+  //     const operation = 'start';
+  //     const result = await environmentService.changeWorkspaceRunState(requestContext, { id, operation });
+  //
+  //     res.status(200).json(result);
+  //   }),
+  // );
+  //
+  // // ===============================================================
+  // //  PUT / (mounted to /api/workspaces/built-in)
+  // // ===============================================================
+  // router.put(
+  //   '/:id/stop',
+  //   wrap(async (req, res) => {
+  //     const requestContext = res.locals.requestContext;
+  //     const id = req.params.id;
+  //     const operation = 'stop';
+  //     const result = await environmentService.changeWorkspaceRunState(requestContext, { id, operation });
+  //
+  //     res.status(200).json(result);
+  //   }),
+  // );
+  //
+  // // ===============================================================
+  // //  DELETE /:id (mounted to /api/workspaces/built-in)
+  // // ===============================================================
+  // router.delete(
+  //   '/:id',
+  //   wrap(async (req, res) => {
+  //     const id = req.params.id;
+  //     const requestContext = res.locals.requestContext;
+  //
+  //     await environmentService.delete(requestContext, { id });
+  //     res.status(200).json({});
+  //   }),
+  // );
+  //
+  // // ===============================================================
+  // //  GET /:id/keypair (mounted to /api/workspaces/built-in)
+  // // ===============================================================
+  // router.get(
+  //   '/:id/keypair',
+  //   wrap(async (req, res) => {
+  //     const requestContext = res.locals.requestContext;
+  //     const id = req.params.id;
+  //
+  //     const result = await environmentKeypairService.mustFind(requestContext, id);
+  //
+  //     res.status(200).json(result);
+  //   }),
+  // );
+  //
+  // // ===============================================================
+  // //  GET /:id/password (mounted to /api/workspaces/built-in)
+  // // ===============================================================
+  // router.get(
+  //   '/:id/password',
+  //   wrap(async (req, res) => {
+  //     const requestContext = res.locals.requestContext;
+  //     const id = req.params.id;
+  //
+  //     const result = await environmentService.getWindowsPasswordData(requestContext, { id });
+  //
+  //     res.status(200).json(result);
+  //   }),
+  // );
+  //
+  // // ===============================================================
+  // //  GET /:id/url (mounted to /api/workspaces/built-in)
+  // // ===============================================================
+  // router.get(
+  //   '/:id/url',
+  //   wrap(async (req, res) => {
+  //     const requestContext = res.locals.requestContext;
+  //     const id = req.params.id;
+  //     const result = await environmentUrlService.get(requestContext, id);
+  //
+  //     res.status(200).json(result);
+  //   }),
+  // );
+  //
+  // // ===============================================================
+  // //  GET /pricing/:type (mounted to /api/workspaces/built-in)
+  // // ===============================================================
+  // router.get(
+  //   '/pricing/:type',
+  //   wrap(async (req, res) => {
+  //     const requestContext = res.locals.requestContext;
+  //     const type = req.params.type;
+  //     const result = await environmentSpotPriceHistoryService.getPriceHistory(requestContext, type);
+  //
+  //     res.status(200).json(result);
+  //   }),
+  // );
 
   return router;
 }
