cc8cd47fefc4441ac636379623034cbe29202680
ealemayehu <eyor.alemayehu@gmail.com>
2021-03-08 11:02:48 -0500

    test: Implemented integration tests for service catalog workspaces (#350)
    * Integration test for update api of workspace-types
    
    * test: Integration test for update api of workspace-types
    
    * Remove erroneously added update method
    
    Instead of adding an update in WorkspaceTypes
    collection the correct way of doing the update is to
    invoke the update in the singular WorkspaceType
    
    * test: implemented additional workspace-types apis
    
    Only remaining API is delete, which is pending on work
    that needs to be done at the parent resource level.
    
    * Applied code review feedback
    
    Ensured for the positive test cases we have workspaces in
    the databases.
    
    * test: Int tests for workspace-types-configuration
    
    Also includes test for delete-workspace , and typo fixes.
    
    * Applied code review feedback
    
    * Implemented remaning workspace-type apis
    
    1. Delete for configurations
    2. Get for workspace-type-candidates
    3. Get for config-vars
    
    * Removed overrides that throw errors
    
    * Empty commit to trigger PR check
    
    * Applied codereview feedback
    
    * Removed erroneously added file
    
    * test: Implemented workspace service-catalog tests
    
    * Fixed lint error
    
    * Refactored method into common file
    
    * Created a template for the default test product
    
    * Added validation logic for default product ID
    
    This is to ensure the product is based on the cloud formation
    template specified in main/integration-tests/support/service-catalog/default-integration-test-product.yml
    
    * Auto create default product
    
    Currently, we are manually creating the service catalog
    product and specifying its IDs in the stage file. Made
    changes to auto create the product in each relevant
    test suite.
    
    * Hard delete service catalog environments
    
    A soft delete is done when removing service catalog environments.
    For integration testing, we need to do a hard removal of the soft
    deleted service catalog environments.
    
    * Made changes per Yanyu's PR feedback
    
    Co-authored-by: Eyor Alemayehu <eyor@amazon.com>
    Co-authored-by: Yanyu Zheng <yz2690@columbia.edu>
    Co-authored-by: Hatim Khan <hatimengineer@gmail.com>

---
+++ main/integration-tests/__test__/api-tests/workspace-service-catalogs/connections/windows-rdp-info-connection.test.js
@@ -0,0 +1,100 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License").
+ *  You may not use this file except in compliance with the License.
+ *  A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+const { runSetup } = require('../../../../support/setup');
+const errorCode = require('../../../../support/utils/error-code');
+
+const {
+  createWorkspaceTypeAndConfiguration,
+} = require('../../../../support/complex/create-workspace-type-and-configuration');
+const {
+  createDefaultServiceCatalogProduct,
+  deleteDefaultServiceCatalogProduct,
+} = require('../../../../support/complex/default-integration-test-product');
+
+describe('Get Windows password for RDP scenario', () => {
+  let setup;
+  let adminSession;
+  let productInfo;
+
+  beforeAll(async () => {
+    setup = await runSetup();
+    adminSession = await setup.defaultAdminSession();
+    productInfo = await createDefaultServiceCatalogProduct(setup);
+  });
+
+  afterAll(async () => {
+    await deleteDefaultServiceCatalogProduct(setup, productInfo);
+    await setup.cleanup();
+  });
+
+  describe('Get Windows password for RDP scenario', () => {
+    it('should fail if user is inactive', async () => {
+      const adminSession2 = await setup.createAdminSession();
+      const workspaceName = setup.gen.string({ prefix: 'workspace-service-catalog-test' });
+      const { workspaceTypeId, configurationId } = await createWorkspaceTypeAndConfiguration(
+        productInfo,
+        adminSession,
+        setup,
+      );
+      const connectionId = setup.gen.string({ prefix: 'workspace-service-catalog-connection-test' });
+
+      const response = await adminSession.resources.workspaceServiceCatalogs.create({
+        name: workspaceName,
+        envTypeId: workspaceTypeId,
+        envTypeConfigId: configurationId,
+      });
+
+      await adminSession2.resources.users.deactivateUser(adminSession2.user);
+
+      await expect(
+        adminSession2.resources.workspaceServiceCatalogs
+          .workspaceServiceCatalog(response.id)
+          .connections()
+          .connection(connectionId)
+          .windowsRdpInfo(),
+      ).rejects.toMatchObject({
+        code: errorCode.http.code.unauthorized,
+      });
+    });
+
+    it('should fail if user is anonymous', async () => {
+      const anonymousSession = await setup.createAnonymousSession();
+      const workspaceName = setup.gen.string({ prefix: 'workspace-service-catalog-test' });
+      const { workspaceTypeId, configurationId } = await createWorkspaceTypeAndConfiguration(
+        productInfo,
+        adminSession,
+        setup,
+      );
+      const connectionId = setup.gen.string({ prefix: 'workspace-service-catalog-connection-test' });
+
+      const response = await adminSession.resources.workspaceServiceCatalogs.create({
+        name: workspaceName,
+        envTypeId: workspaceTypeId,
+        envTypeConfigId: configurationId,
+      });
+
+      await expect(
+        anonymousSession.resources.workspaceServiceCatalogs
+          .workspaceServiceCatalog(response.id)
+          .connections()
+          .connection(connectionId)
+          .windowsRdpInfo(),
+      ).rejects.toMatchObject({
+        code: errorCode.http.code.badImplementation,
+      });
+    });
+  });
+});
