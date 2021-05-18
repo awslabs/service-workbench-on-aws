740235a97bf42477e731471f69fe78417c8ddd8e
Tim Nguyen <thingut@amazon.com>
2021-05-18 18:00:26 -0400

    chore: Add GH action check for dependency vulnerabilities

---
+++ scripts/check-dependency-vulnerabilities.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+pnpm audit --json --audit-level high --prod > audit-results.json
+high_vul=$(cat audit-results.json | jq '.metadata.vulnerabilities.high')
+critical_vul=$(cat audit-results.json | jq '.metadata.vulnerabilities.critical')
+
+high_vul_threshold=2
+critical_vul_threshold=0
+
+if (($high_vul > $high_vul_threshold)) || (($critical_vul > critical_vul_threshold)); then
+  exit 1
+else
+  exit 0
+fi
