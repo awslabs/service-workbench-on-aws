fcee65683a8f72e029cd7e4aebca6e7482eacbda
Tim Nguyen <thingut@amazon.com>
2021-05-18 18:13:14 -0400

    Working

--- scripts/check-dependency-vulnerabilities.sh
+++ scripts/check-dependency-vulnerabilities.sh
@@ -3,7 +3,7 @@ pnpm audit --json --audit-level high --prod > audit-results.json
 high_vul=$(cat audit-results.json | jq '.metadata.vulnerabilities.high')
 critical_vul=$(cat audit-results.json | jq '.metadata.vulnerabilities.critical')
 
-high_vul_threshold=1
+high_vul_threshold=2
 critical_vul_threshold=0
 
 if (($high_vul > $high_vul_threshold)) || (($critical_vul > critical_vul_threshold)); then
