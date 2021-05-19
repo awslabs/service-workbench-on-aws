b003eedb6b829ea98f037aa2ec89668aa0343361
Tim Nguyen <thingut@amazon.com>
2021-05-19 10:39:23 -0400

    Add readable format in script

--- scripts/check-dependency-vulnerabilities.sh
+++ scripts/check-dependency-vulnerabilities.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Display human readable result
+pnpm audit --audit-level high --prod;
+
+# Parse results
 pnpm audit --json --audit-level high --prod > audit-results.json
 high_vul=$(cat audit-results.json | jq '.metadata.vulnerabilities.high')
 critical_vul=$(cat audit-results.json | jq '.metadata.vulnerabilities.critical')
