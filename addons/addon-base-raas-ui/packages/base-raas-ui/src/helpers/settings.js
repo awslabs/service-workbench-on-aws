4da9f6b046c95b4e5e233f6c01fe9de9c2200539
Xian Zhang <zhaxan@amazon.com>
2021-06-16 09:37:59 -0400

    fix: add enableEgressStore in setting file

--- addons/addon-base-raas-ui/packages/base-raas-ui/src/helpers/settings.js
+++ addons/addon-base-raas-ui/packages/base-raas-ui/src/helpers/settings.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 const enableBuiltInWorkspaces = process.env.REACT_APP_ENABLE_BUILT_IN_WORKSPACES === 'true';
+const enableEgressStore = process.env.REACT_APP_ENABLE_EGRESS_STORE === 'true';
 
-export { enableBuiltInWorkspaces };
+export { enableBuiltInWorkspaces, enableEgressStore };
