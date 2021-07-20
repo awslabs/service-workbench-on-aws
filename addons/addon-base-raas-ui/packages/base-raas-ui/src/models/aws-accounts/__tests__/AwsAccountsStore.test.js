0b9c9f15f0ab2f658d780afe7ca6a4e9f830d818
ahl27 <lakaidan@amazon.com>
2021-07-20 12:14:12 -0400

    forgot to save one file

--- addons/addon-base-raas-ui/packages/base-raas-ui/src/models/aws-accounts/__tests__/AwsAccountsStore.test.js
+++ addons/addon-base-raas-ui/packages/base-raas-ui/src/models/aws-accounts/__tests__/AwsAccountsStore.test.js
@@ -121,7 +121,7 @@ describe('AwsAccountsStore', () => {
   describe('updateAccount', () => {
     it('should try to update the account with updated permissions', async () => {
       const erroredAcct = { id: 'testid', permissionsStatus: 'CURRENT' };
-      const newPermRetVal = { newStatus: { testid: 'NEEDSUPDATE' } };
+      const newPermRetVal = { newStatus: { testid: 'NEEDS_UPDATE' } };
       getAllAccountsPermissionStatus.mockResolvedValue(newPermRetVal);
       await store.load();
       await store.updateAwsAccount(erroredAcct.id, erroredAcct);
