7284713ef7474c11bfa1afe4c00ace345713f937
Jeet <68876606+jn1119@users.noreply.github.com>
2021-10-14 13:08:43 -0400

    docs: Minor comment updates and add copyright headers (#751)

--- main/solution/post-deployment/bool-to-str.js
+++ main/solution/post-deployment/bool-to-str.js
@@ -1,3 +1,17 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License").
+ *  You may not use this file except in compliance with the License.
+ *  A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
 // Adapted from: https://stackoverflow.com/questions/65832576/converting-ssm-value-to-number
 
 // To convert a boolean value to a string to be used by the serverless framework
