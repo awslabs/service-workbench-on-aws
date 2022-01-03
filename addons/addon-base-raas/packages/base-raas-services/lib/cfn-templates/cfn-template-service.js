93dc4658af2f6bbf9286cc1b882b7f944f365d0e
Tim Nguyen <nguyen102@users.noreply.github.com>
2022-01-03 17:07:42 -0500

    fix: Allow onboarding member account in non AppStream supported regions (#844)

--- addons/addon-base-raas/packages/base-raas-services/lib/cfn-templates/cfn-template-service.js
+++ addons/addon-base-raas/packages/base-raas-services/lib/cfn-templates/cfn-template-service.js
@@ -15,6 +15,13 @@
 
 const _ = require('lodash');
 const Service = require('@aws-ee/base-services-container/lib/service');
+const { yamlParse } = require('yaml-cfn');
+const YAML = require('yaml');
+const ObjectPath = require('object-path');
+
+const settingKeys = {
+  isAppStreamEnabled: 'isAppStreamEnabled',
+};
 
 class CfnTemplateService extends Service {
   constructor() {
@@ -46,8 +53,76 @@ class CfnTemplateService extends Service {
     this.store.push({ key: name, value: yaml });
   }
 
+  // Return attributesToDelete if the attribute is an "IF" statement with the condition 'isAppStreamAndCustomDomain' or 'isAppStream' and the else clause is 'AWS::NoValue'
+  getResourceAttributesToDelete(doc, policiesPath) {
+    const attributesToDelete = [];
+    if (_.get(doc, policiesPath)) {
+      const policies = _.get(doc, policiesPath);
+      let index;
+      for (let i = 0; i < policies.length; i += 1) {
+        // If policies with condition of 'isAppStream' or 'isAppStreamAndCustomDomain'
+        if (policies[i]['Fn::If'] && ['isAppStream', 'isAppStreamAndCustomDomain'].includes(policies[i]['Fn::If'][0])) {
+          const elseClause = _.get(policies, `${i}.Fn::If.2.Ref`);
+          if (elseClause === 'AWS::NoValue') {
+            index = i;
+          }
+        }
+      }
+      if (index) {
+        attributesToDelete.push(`${policiesPath}.${index}`);
+      }
+    }
+    return attributesToDelete;
+  }
+
   async getTemplate(name) {
     const entry = _.find(this.store, ['key', name]);
+    const isAppStreamEnabled = await this.settings.getBoolean(settingKeys.isAppStreamEnabled);
+    if (!isAppStreamEnabled && name === 'onboard-account' && entry) {
+      const doc = yamlParse(entry.value);
+      const resourcesToDelete = [];
+      let resourceAttributeToDelete = [];
+      Object.keys(doc.Resources).forEach(resource => {
+        if (
+          ['isAppStreamAndCustomDomain', 'isAppStream'].includes(
+            doc.Resources[resource].Condition && doc.Resources[resource].Condition,
+          )
+        ) {
+          resourcesToDelete.push(resource);
+        }
+        resourceAttributeToDelete = resourceAttributeToDelete.concat(
+          this.getResourceAttributesToDelete(doc, `Resources.${resource}.Properties.Policies`),
+        );
+        resourceAttributeToDelete = resourceAttributeToDelete.concat(
+          this.getResourceAttributesToDelete(doc, `Resources.${resource}.Properties.PolicyDocument.Statement`),
+        );
+      });
+
+      resourcesToDelete.forEach(resource => {
+        delete doc.Resources[resource];
+      });
+      resourceAttributeToDelete.forEach(path => {
+        ObjectPath.del(doc, path);
+      });
+
+      const outputsToDelete = [];
+      Object.keys(doc.Outputs).forEach(output => {
+        if (
+          ['isAppStreamAndCustomDomain', 'isAppStream'].includes(
+            doc.Outputs[output].Condition && doc.Outputs[output].Condition,
+          )
+        ) {
+          outputsToDelete.push(output);
+        }
+      });
+      outputsToDelete.forEach(output => {
+        delete doc.Outputs[output];
+      });
+
+      const onboardYaml = new YAML.Document();
+      onboardYaml.contents = doc;
+      return onboardYaml.toString();
+    }
     return entry ? entry.value : undefined;
   }
 }
