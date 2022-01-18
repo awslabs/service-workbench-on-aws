3e6d020e2e34190ceb0e3f5bb7b651edea96ef5f
Tim Nguyen <thingut@amazon.com>
2022-01-18 17:22:43 -0500

    Update get-info.sh

--- scripts/get-info.sh
+++ scripts/get-info.sh
@@ -76,12 +76,9 @@ function get_info() {
   # only show profile and root password when running in an interactive terminal
   if [ -t 1 ] ; then
     [ -z "${aws_profile}" ] || echo "AWS Profile                        : ${aws_profile}"
-    root_passwd="$(${root_psswd_cmd})"
-    echo "Root Password                      : ${root_passwd}"
     native_admin_psswd="$(${native_admin_psswd_cmd})"
     echo "Temporary Native Pool Password     : ${native_admin_psswd}"
   else
-    echo "Root Password             : execute ${root_psswd_cmd}"
     echo "Temporary Native Pool Password     : execute ${native_admin_psswd_cmd}"
   fi
 
