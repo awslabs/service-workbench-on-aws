0881e212b658f007cea796221fc5823f9d10cdb8
Tim Nguyen <thingut@amazon.com>
2022-01-18 10:40:26 -0500

    chore: remove create internal auth user api

--- addons/addon-base-raas/packages/base-raas-rest-api/lib/controllers/users-controller.js
+++ addons/addon-base-raas/packages/base-raas-rest-api/lib/controllers/users-controller.js
@@ -31,17 +31,18 @@ async function configure(context) {
     }),
   );
 
+  // TODO: Do we want to keep this API and support creating Cognito users? Currently API only support creating internal users
   // ===============================================================
   //  POST / (mounted to /api/users)
   // ===============================================================
-  router.post(
-    '/',
-    wrap(async (req, res) => {
-      const requestContext = res.locals.requestContext;
-      const createdUser = await userService.createUser(requestContext, req.body);
-      res.status(200).json(createdUser);
-    }),
-  );
+  // router.post(
+  //   '/',
+  //   wrap(async (req, res) => {
+  //     const requestContext = res.locals.requestContext;
+  //     const createdUser = await userService.createUser(requestContext, req.body);
+  //     res.status(200).json(createdUser);
+  //   }),
+  // );
 
   // ===============================================================
   //  POST / (mounted to /api/users/bulk)
@@ -74,22 +75,23 @@ async function configure(context) {
     }),
   );
 
+  // TODO: Do we want to support changing user passwords? Currently this API is only supported for internal Auth
   // ===============================================================
   //  PUT /:username/password (mounted to /api/users)
   //  In this case it is relevant to identify user by username/authProvider
   // ===============================================================
-  router.put(
-    '/:username/password',
-    wrap(async (req, res) => {
-      const requestContext = res.locals.requestContext;
-      const username = req.params.username;
-      const { password } = req.body;
-
-      // Save password salted hash for the user in internal auth provider (i.e., in passwords table)
-      await dbPasswordService.savePassword(requestContext, { username, password });
-      res.status(200).json({ username, message: `Password successfully updated for user ${username}` });
-    }),
-  );
+  // router.put(
+  //   '/:username/password',
+  //   wrap(async (req, res) => {
+  //     const requestContext = res.locals.requestContext;
+  //     const username = req.params.username;
+  //     const { password } = req.body;
+  //
+  //     // Save password salted hash for the user in internal auth provider (i.e., in passwords table)
+  //     await dbPasswordService.savePassword(requestContext, { username, password });
+  //     res.status(200).json({ username, message: `Password successfully updated for user ${username}` });
+  //   }),
+  // );
 
   // ===============================================================
   //  DELETE /:uid (mounted to /api/users)
