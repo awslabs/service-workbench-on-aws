9499bd30076b97fb72bad1f6b3abfbced7a3b441
dengmingtong <dengmingtong@hotmail.com>
2022-02-13 15:11:01 +0800

    fix stop and start linux and sagemaker workspace type

--- addons/addon-environment-sc-api/packages/environment-sc-workflow-steps/lib/steps/start-ec2-environment/start-ec2-environment-sc.js
+++ addons/addon-environment-sc-api/packages/environment-sc-workflow-steps/lib/steps/start-ec2-environment/start-ec2-environment-sc.js
@@ -100,7 +100,8 @@ class StartEc2EnvironmentSc extends StepBase {
       this.payload.string('roleExternalId'),
     ]);
 
-    const sts = new aws.sdk.STS();
+    // const sts = new aws.sdk.STS();
+    const sts = new aws.sdk.STS({ apiVersion: '2011-06-15', stsRegionalEndpoints: 'regional' });
     const {
       Credentials: { AccessKeyId: accessKeyId, SecretAccessKey: secretAccessKey, SessionToken: sessionToken },
     } = await sts
--- addons/addon-environment-sc-api/packages/environment-sc-workflow-steps/lib/steps/stop-sagemaker-environment/stop-sagemaker-environment-sc.js
+++ addons/addon-environment-sc-api/packages/environment-sc-workflow-steps/lib/steps/stop-sagemaker-environment/stop-sagemaker-environment-sc.js
@@ -107,7 +107,8 @@ class StopSagemakerEnvironmentSc extends StepBase {
       this.payload.string('roleExternalId'),
     ]);
 
-    const sts = new aws.sdk.STS();
+    // const sts = new aws.sdk.STS();
+    const sts = new aws.sdk.STS({ apiVersion: '2011-06-15', stsRegionalEndpoints: 'regional' });
     const {
       Credentials: { AccessKeyId: accessKeyId, SecretAccessKey: secretAccessKey, SessionToken: sessionToken },
     } = await sts
