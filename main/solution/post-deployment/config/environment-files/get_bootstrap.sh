fc25b15666b9162a6dad50bd269b0a1a18aceb6f
dengmingtong <dengmingtong@hotmail.com>
2022-02-18 08:11:56 +0800

    fix hongkong region issue in get_bootstrap.sh file

--- main/solution/post-deployment/config/environment-files/get_bootstrap.sh
+++ main/solution/post-deployment/config/environment-files/get_bootstrap.sh
@@ -6,7 +6,8 @@ INSTALL_DIR="/usr/local/share/workspace-environment"
 
 # Download instance files and execute bootstrap script
 sudo mkdir "$INSTALL_DIR"
-sudo aws s3 sync "$bootstrap_s3_location" "$INSTALL_DIR"
+# sudo aws s3 sync "$bootstrap_s3_location" "$INSTALL_DIR"
+sudo aws s3 sync "$bootstrap_s3_location" "$INSTALL_DIR" --region "ap-east-1"
 
 bootstrap_script="$INSTALL_DIR/bootstrap.sh"
 if [ -s "$bootstrap_script" ]
