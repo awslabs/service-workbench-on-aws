8c6f0ae8218c63867aafaf50922c9861e193682d
jxuamazon <48922441+jxuamazon@users.noreply.github.com>
2023-03-12 22:30:10 -0400

    Update StudyPermissionsStore.js
    not to update all users who have the permission. only process the delta

--- addons/addon-base-raas-ui/packages/base-raas-ui/src/models/studies/StudyPermissionsStore.js
+++ addons/addon-base-raas-ui/packages/base-raas-ui/src/models/studies/StudyPermissionsStore.js
@@ -65,7 +65,7 @@ const StudyPermissionsStore = BaseStore.named('StudyPermissionsStore')
           // Set selected users as "usersToAdd" (API is idempotent)
           // And remove staleUserIds from usersToAdd list
           updateRequest.usersToAdd.push(
-            ..._.differenceWith(selectedUserIds[type], staleUserIds[type], _.isEqual).map(userToRequestFormat),
+            ..._.differenceWith(selectedUserIds[type], self.studyPermissions[`${type}Users`], _.isEqual).map(userToRequestFormat),
           );
 
           // Set removed users as "usersToRemove"
